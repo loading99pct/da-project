d9b958632f421cda9e9f359ed5c7169d6e5e6611
loading99pct <tpmail.310@gmail.com>
2019-12-19 18:33:52 -0500

    Delete init-node.bash

--- init-node.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~
-[ -d "./da-project" ] && rm -rf ./da-project
-git clone https://github.com/loading99pct/da-project.git
-chmod 777 ./da-project/run-node.bash
-sh ./da-project/run-node.bash "$@"
