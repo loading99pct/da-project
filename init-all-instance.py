7f32f435c845b9b07821fb1068dca31ae989534a
loading99pct <tpmail.310@gmail.com>
2019-12-19 22:20:06 -0500

    Update init-all-instance.py

--- init-all-instance.py
+++ init-all-instance.py
@@ -5,12 +5,31 @@ import json
 
 def writeAwsRunInstFile(nodeName: str, fileName: str):
 	with open(fileName, "w") as f:
+		headPart = """Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+\n"""
 		bashOnRun = "\n".join(["#!/bin/bash",
 			"""echo "running" | tee logfile.txt""", 
 			"git clone https://github.com/loading99pct/da-project.git",
-			"chmod 777 ~/da-project/run-node.bash > changeMode.txt ", 
-			"sh ~/da-project/run-node.bash {} > runShell.txt".format(nodeName)])
-		f.write(bashOnRun)
+			"chmod 777 /home/ec2-user/da-project/run-node.bash > changeMode.txt ", 
+			"sh /home/ec2-user/da-project/run-node.bash {} > runShell.txt".format(nodeName)])
+		f.write(headPart + bashOnRun)
 
 def parseNewInstFeedbackToIp(outputMsg: str):
     jsonObj = json.loads(outputMsg)
