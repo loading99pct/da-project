678aa740c5ba875cc7a87a9fcb9b151ba675cf61
loading99pct <tpmail.310@gmail.com>
2019-12-19 19:11:25 -0500

    Update run-manager.bash

--- run-manager.bash
+++ run-manager.bash
@@ -2,7 +2,7 @@
 cd ~
 [ -d "./da-project" ] && rm -rf ./da-project 
 git clone https://github.com/loading99pct/da-project.git  
-chmod 777 ./da-project/run-node.bash
+chmod 777 ./da-project/run-manager.bash
 PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
 cd ~/da-project
 python3 -m da -f -n ManagerNode -i ./myCode.da "$@"
