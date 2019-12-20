24cf807853e0d34219702aabc78cae0f6e558b2b
loading99pct <tpmail.310@gmail.com>
2019-12-19 20:10:42 -0500

    Update init-all-instance.py

--- init-all-instance.py
+++ init-all-instance.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
 
 		if not appendQ:
 			with open("./newDaAddr.config", "w") as f:
-				f.writelines(daAddrL + "\n")
+				f.writelines("\n".join(daAddrL))
 		else:
 			with open("./newDaAddr.config", "a") as f:
 				f.writeline(daAddr + "\n")
