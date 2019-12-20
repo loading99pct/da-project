9e995c947430808e2f909bf954a0f97bfee72bbf
loading99pct <tpmail.310@gmail.com>
2019-12-19 19:02:16 -0500

    Create run-manager.bash

---
+++ run-manager.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~
+[ -d "./da-project" ] && rm -rf ./da-project 
+git clone https://github.com/loading99pct/da-project.git  
+chmod 777 ./da-project/run-node.bash
+PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
+cd ~/da-project
+python3 -m da -f -n ManagerNode -i ./myCode.da "$@"
