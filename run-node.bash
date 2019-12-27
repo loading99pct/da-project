f5aeef6e6b4b410f60af47343b59c951f798be98
loading99pct <tpmail.310@gmail.com>
2019-12-26 23:45:05 -0500

    Delete run-node.bash

--- run-node.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
-chmod 777 /home/ec2-user/da-project/myCode.da
-python3 -m da -f --message-buffer-size 1024000 -n $1 -D /home/ec2-user/da-project/myCode.da
