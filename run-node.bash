c3c6fef80bcdaf7da501d11ba3617eb5cef344eb
loading99pct <tpmail.310@gmail.com>
2019-12-26 23:31:17 -0500

    Update run-node.bash

--- run-node.bash
+++ run-node.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
-python3 -m da -f --message-buffer-size 1024000 -n $1 -D ./da-project/myCode.da
+python3 -m da -f --message-buffer-size 1024000 -n $1 -D /home/ec2-user/da-project/myCode.da
