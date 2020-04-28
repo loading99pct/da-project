b65f84b04ac8454f4a59f0f09c7100c1c2df517d
loading99pct <tpmail.310@gmail.com>
2020-04-28 11:09:35 -0400

    Update run-node.bash

--- run-node.bash
+++ run-node.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
 cp ~/da-project/sock.py ~/da/distalgo/da/transport/sock.py
+rm -f ~/myCode.da.log
 chmod 777 /home/ec2-user/da-project/myCode.da
 python3 -m da -f --message-buffer-size 4096000 -n $1 -D /home/ec2-user/da-project/myCode.da
