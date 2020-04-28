526c62119f3d699e77efa3fa431f3765434e0427
loading99pct <tpmail.310@gmail.com>
2020-04-28 08:41:09 -0400

    Update reload_run_node.bash

--- reload_run_node.bash
+++ reload_run_node.bash
@@ -2,5 +2,5 @@
 [ -d "./da-project" ] && rm -rf ./da-project 
 git clone https://github.com/loading99pct/da-project.git  
 export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
-chmod 777 /home/ec2-user/da-project/myCode.da
-python3 -m da -f --message-buffer-size 1024000 -n $1 -D /home/ec2-user/da-project/myCode.da
+chmod 777 /home/ec2-user/da-project/run-node.da
+sh ./da-project/run-node.bash "$@"
