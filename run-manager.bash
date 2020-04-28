dd90941fba0216f9a2385d3e2626356796ae2e42
loading99pct <tpmail.310@gmail.com>
2020-04-27 20:46:00 -0400

    Update run-manager.bash

--- run-manager.bash
+++ run-manager.bash
@@ -3,6 +3,6 @@ cd ~
 [ -d "./da-project" ] && rm -rf ./da-project 
 git clone https://github.com/loading99pct/da-project.git  
 chmod 777 ./da-project/run-manager.bash
-PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
+export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
 cd ~/da-project
 python3 -m da -f -n ManagerNode --message-buffer-size 1024000 -i ./myCode.da "$@"
