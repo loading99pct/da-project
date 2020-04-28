ffc2ba72237487229b5fc953e40f89f56a4420d4
loading99pct <tpmail.310@gmail.com>
2020-04-28 09:27:35 -0400

    Update run-manager.bash

--- run-manager.bash
+++ run-manager.bash
@@ -2,6 +2,7 @@
 cd ~
 [ -d "./da-project" ] && rm -rf ./da-project 
 git clone https://github.com/loading99pct/da-project.git  
+cp ./da-project/sock.py ./da/distalgo/da/transport/sock.py
 chmod 777 ./da-project/run-manager.bash
 export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
 cd ~/da-project
