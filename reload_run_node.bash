afab65bb5f982b4849681a73e852218e6c97a6bf
loading99pct <tpmail.310@gmail.com>
2020-04-28 08:42:44 -0400

    Update reload_run_node.bash

--- reload_run_node.bash
+++ reload_run_node.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 [ -d "./da-project" ] && rm -rf ./da-project 
 git clone https://github.com/loading99pct/da-project.git  
-export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
 chmod 777 /home/ec2-user/da-project/run-node.da
 sh ./da-project/run-node.bash "$@"
