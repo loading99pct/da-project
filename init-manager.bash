c1860515dc94db61ba040e68b9a523ca5a733c54
loading99pct <tpmail.310@gmail.com>
2020-04-28 00:38:18 -0400

    Update init-manager.bash

--- init-manager.bash
+++ init-manager.bash
@@ -8,5 +8,6 @@ export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
 cd ~
 [ -d "./da-project" ] && rm -rf ./da-project 
 git clone https://github.com/loading99pct/da-project.git  
+chmod 777 ./da-project/run-manager.bash
 mkdir ~/inst-bash
 mkdir ~/.aws
