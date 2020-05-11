42682ea5b9fd38d3cf6cc71d69d392577d1de054
loading99pct <tpmail.310@gmail.com>
2020-05-11 00:22:49 -0400

    Create run-measurement.bash

---
+++ run-measurement.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~
+[ -d "./da-project" ] && rm -rf ./da-project
+git clone https://github.com/loading99pct/da-project.git
+export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
+chmod 777 ./da-project/run-manager.bash
+cd ./da-project
+python3 measurement.py
