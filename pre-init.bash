cd1a02e341ea87d6220b492a118978e8e6ae4a8d
loading99pct <tpmail.310@gmail.com>
2020-05-11 00:14:24 -0400

    Delete pre-init.bash

--- pre-init.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~
-git clone https://github.com/loading99pct/da-project.git
-cp ./da-project/myCode.da ./da/distalgo/da/examples/mytest/ct.da
-cp ./da-project/pre-init.da ./pre-init.da
-chmod 777 ./pre-init
