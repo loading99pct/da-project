4ba4e43cc29ae43ea5437994288fc44d123e0f7a
loading99pct <tpmail.310@gmail.com>
2020-05-11 00:10:37 -0400

    Delete run-manager.bash

--- run-manager.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~
-[ -d "./da-project" ] && rm -rf ./da-project 
-git clone https://github.com/loading99pct/da-project.git  
-cp ./da-project/sock.py ./da/distalgo/da/transport/sock.py
-chmod 777 ./da-project/run-manager.bash
-export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
-cd ~/da-project
-python3 -m da -f -n ManagerNode --message-buffer-size 4096000 -i ./myCode.da "$@"
