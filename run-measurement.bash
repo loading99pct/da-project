be8b1515b3f4ad3a973768a0b7da9aa411613b1d
loading99pct <tpmail.310@gmail.com>
2020-05-25 19:38:18 -0400

    Update run-measurement.bash

--- run-measurement.bash
+++ run-measurement.bash
@@ -3,6 +3,6 @@ cd ~
 [ -d "./da-project" ] && rm -rf ./da-project
 git clone https://github.com/loading99pct/da-project.git
 export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
-chmod 777 ./da-project/run-manager.bash
+chmod 777 ./da-project/run-measurement.bash
 cd ./da-project
 python3 measurement.py
