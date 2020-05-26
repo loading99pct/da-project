c74b83e3c70f0d9b601601ab467682399ac8239a
loading99pct <tpmail.310@gmail.com>
2020-05-25 20:36:57 -0400

    Update init-manager.bash

--- init-manager.bash
+++ init-manager.bash
@@ -8,7 +8,7 @@ export PYTHONPATH="/home/ec2-user/da/distalgo":${PYTHONPATH}
 cd ~
 [ -d "./da-project" ] && rm -rf ./da-project 
 git clone https://github.com/loading99pct/da-project.git  
-chmod 777 ./da-project/run-manager.bash
+chmod 777 ./da-project/run-measurement.bash
 mkdir ~/inst-bash
 mkdir ~/.aws
 echo '' >> /etc/security/limits.conf
